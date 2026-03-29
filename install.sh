315ea4fbfe466c9c11b99e1f801bc700486968db
duckyfuz <108561447+duckyfuz@users.noreply.github.com>
2026-03-29 11:17:04 +0800

    add omzsh config

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+OS="$(uname -s)"
+
+echo "==> Installing core packages..."
+if [[ "$OS" == "Darwin" ]]; then
+    brew install stow neovim tmux
+elif [[ "$OS" == "Linux" ]]; then
+    sudo apt-get update -qq
+    sudo apt-get install -y stow neovim tmux curl
+fi
+
+echo "==> Installing oh-my-zsh..."
+if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+echo "==> Installing zsh plugins..."
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
+[[ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]] &&
+    git clone --quiet https://github.com/zsh-users/zsh-autosuggestions \
+        "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+
+[[ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]] &&
+    git clone --quiet https://github.com/zsh-users/zsh-syntax-highlighting \
+        "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+
+echo "==> Stowing dotfiles..."
+stow */
+
+echo "Done! Start a new shell: exec zsh"
---
+++ zsh/.zprofile
@@ -0,0 +1,10 @@
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Created by `pipx` on 2025-06-13 16:46:45
+export PATH="$PATH:/Users/kenf/.local/bin"
+
+
+# Added by Toolbox App
+export PATH="$PATH:/Users/kenf/Library/Application Support/JetBrains/Toolbox/scripts"
+
